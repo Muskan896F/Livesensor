0468aad5dbdfa77833fe28ab1dcd646211c52a09
Muskan986F <muskan17ara@gmail.com>
2024-05-17 23:55:25 +0530

    Add all changes to project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+#from typing import List
+def get_reuirements()->list[str]:
+    requirements_list = list[str] =[]
+    return requirements_list
+
+setup(
+
+name = 'sensor',
+version = "0.0.1",
+author = "Muskan",
+author_email = "muskan17ara@gmail.com",
+packages = get_reuirements(),
+
+
+
+
+
+)
